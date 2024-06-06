68e9bd421537c370a2cc43cae48d758ddd7b1d1b
mathgvlt0 <mgayvallet@edenschool.fr>
2024-06-06 17:18:53 +0200

    starting play menu

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
     const mainContainer = document.getElementById('mainContainer');
     const optionsContainer = document.getElementById('optionsContainer');
     const quizContainer = document.getElementById('quizContainer');
+    const nextBtn = document.getElementById('nextBtn');
+    const questionCounter = document.getElementById('questionCounter');
+    const flagImage = document.querySelector('.flag-image');
+    const scoreDisplay = document.getElementById('score');
+
+    let score = 0;
+    let questionNumber = 1;
 
     optionsBtn.addEventListener('click', function() {
         mainContainer.style.display = 'none';
@@ -16,4 +23,50 @@ document.addEventListener("DOMContentLoaded", function() {
         optionsContainer.style.display = 'none';
         mainContainer.style.display = 'block';
     });
-})
+
+    playBtn.addEventListener('click', function() {
+        mainContainer.style.display = 'none';
+        quizContainer.style.display = 'block';
+    });
+
+    backToMainBtn.addEventListener('click', function() {
+        quizContainer.style.display = 'none';
+        mainContainer.style.display = 'block';
+    });
+
+    const quizOptions = document.querySelectorAll('.quiz-option');
+    quizOptions.forEach(option => {
+        option.addEventListener('click', function() {
+            if (!document.querySelector('.quiz-option[disabled]')) {
+                const isCorrect = this.dataset.correct === "true";
+
+                quizOptions.forEach(option => {
+                    option.disabled = true; 
+                    if (option.dataset.correct === "true") {
+                        option.classList.add('correct');
+                    }
+                });
+
+                if (isCorrect) {
+                    this.classList.add('correct');
+                    score++;
+                } else {
+                    this.classList.add('incorrect');
+                }
+
+                scoreDisplay.textContent = score;
+                nextBtn.style.display = 'block'; 
+                nextBtn.addEventListener('click', nextQuestion); 
+            }
+        });
+    });
+
+    function nextQuestion() {
+        questionNumber++;
+        questionCounter.textContent = `${questionNumber}/25`;
+        nextBtn.removeEventListener('click', nextQuestion);
+        flagImage.src = 'drapeaux/suisse.png';
+        nextBtn.style.display = 'none'; 
+        
+    }
+});
